8e4e9e06cc9e7dab5b2deb62cced0015a23dba73
greenvegetal <Azhxta@gmail.com>
2020-04-24 13:30:01 +0600

    lots of new additions

--- script.js
+++ script.js
@@ -7,35 +7,69 @@ const finish = document.querySelector('.finish');
 let playerScore = 0;
 let computerScore = 0;
 
+
+// computerselection by rng
+
 const items = ["Rock", "Paper" , "Scissors"];
 let computerChoice = items[Math.floor(Math.random() * items.length)];
 
-let winHTML = `<p>Congrats! You win!
-            Player Score : ${playerScore} <br>
-            Computer Score : ${computerScore} <p>
-`;
-let loseHTML = `<p>Uh oh, You lose!
-            Player Score : ${playerScore} <br>
-            Computer Score : ${computerScore} <p>
-`;
-let tieHTML = `<p>Err, it's a tie!
-            Player Score : ${playerScore} <br>
-            Computer Score : ${computerScore} <p>
-`;
 
+// These are the functions which updates the dom about the winner 
+// and of the points of the players
 
+const winHTML = function(){
+
+    let HTML = `<p>Congrats! You win!
+            Player Score : ${playerScore} <br>
+            Computer Score : ${computerScore} </p>`;
+
+    if(playerScore > 0) {
+        scoreArea.innerHTML = HTML;
+    }
+};
+const loseHTML = function(){
+
+    let HTML = `<p>Uh oh, You lose!
+    Player Score : ${playerScore} <br>
+    Computer Score : ${computerScore} </p>`;
+
+    if(computerScore > 0) {
+        scoreArea.innerHTML = HTML;
+    }
+};
+
+const tieHTML = function(){
+
+    let HTML = `<p>Err, it's a tie!
+    Player Score : ${playerScore} <br>
+    Computer Score : ${computerScore} </p>`;
+
+    if(playerScore) {
+        scoreArea.innerHTML = HTML;
+    }
+};
+
+// Displaying the finish button to decide winner
+
+function displayFinishBtn() {
+    if (playerScore > 0  || computerScore > 0) {
+        finish.style.display = "block";
+    }
+}
+
+//Playing the game, this is the function that is being attached as an event 
+// to the variables rock, paper and scissors
 
 function playGame(e) {
     if (e.target.textContent === "Rock") {
         if (computerChoice === "Scissors") {
             playerScore += 10;
-            scoreArea.innerHTML = winHTML;
+            winHTML();
         } else if (computerChoice === "Paper") {
             computerScore += 10;
-            scoreArea.innerHTML = loseHTML;
-
+            loseHTML();
         } else {
-            scoreArea.innerHTML = tieHTML;
+            tieHTML();
         }
         
     } 
@@ -44,14 +78,14 @@ function playGame(e) {
     else if (e.target.textContent === "Paper") {
         if (computerChoice === "Rock") {
             playerScore += 10;
-            scoreArea.innerHTML = winHTML;
+            winHTML();
 
         } else if (computerChoice === "Scissors") {
             computerScore += 10;
-            scoreArea.innerHTML = loseHTML;
+            loseHTML();
 
         } else {
-            scoreArea.innerHTML = tieHTML;
+            tieHTML();
         }
         
     } 
@@ -60,22 +94,46 @@ function playGame(e) {
     else if (e.target.textContent === "Scissors") {
         if (computerChoice === "Paper") {
             playerScore += 10;
-            scoreArea.innerHTML = winHTML;
+            winHTML();
 
         } else if (computerChoice === "Rock") {
             computerScore += 10;
-            scoreArea.innerHTML = loseHTML;
+            loseHTML();
 
         } else {
-            scoreArea.innerHTML = tieHTML;
+            tieHTML();
         }
         
     } 
 
-console.log(playerScore, computerScore);
+displayFinishBtn();
 
 }
+
+// attaching event to the finish button to decide the winner
+
+function whoWon() {
+    let differenceOne = playerScore - computerScore;
+    let differenceTwo = computerScore - playerScore;
+
+    if (playerScore > computerScore) {
+        scoreArea.innerHTML = `
+        <p class="result"> Congrats! You beat the computer by ${differenceOne} points! </p>
+        `;
+    } else if (computerScore > playerScore){
+        scoreArea.innerHTML = `
+        <p class="result"> Ouch! You are beaten by the computer by ${differenceTwo} points! </p>
+        `;
+    } else {
+        scoreArea.innerHTML = `
+        <p class="result"> Aww its a tie! Be friends I guess? </p>
+        `;
+    }
+}
+
+
 
 rock.addEventListener('click', playGame);
 paper.addEventListener('click', playGame);
 scissors.addEventListener('click', playGame);
+finish.addEventListener('click', whoWon)
